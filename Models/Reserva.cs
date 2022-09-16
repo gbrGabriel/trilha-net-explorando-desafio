ec336c7b486e1ca130ac8dc487fb9c4a911826c8
Gabriel <gabrielgbr.contato@hotmail.com>
2022-09-16 19:08:24 -0300

    Arquivos para o projeto do desafio

---
+++ Models/Reserva.cs
@@ -0,0 +1,42 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count >= Suite.Capacidade) throw new Exception("Capacidade excedida");
+
+            Hospedes = hospedes;
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10) valor -= ((10.0m / 100.0m) * valor);
+
+            return valor;
+        }
+    }
+}
